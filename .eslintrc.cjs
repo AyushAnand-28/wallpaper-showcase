9d17089efdadf7fd8c85c66dc7519fc91ff682c4
AyushAnand-28 <sirayushanand@gmail.com>
2026-02-10 14:45:22 +0530

    style: add eslint and prettier configuration for code quality

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  env: { node: true, es2022: true, browser: true },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
+};
